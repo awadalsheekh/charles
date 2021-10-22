e968334455ab3d6717e6f889c4a36bc73f7a35c2
awadalsheekh <awadalsheekh@gmail.com>
2021-10-22 10:47:48 +0200

    Create folders and files : images, pages(home page, gallery, prices and services, contact), css files(home-page-style.css, main.css, portfolio-style.css), js file(portfolio-olio-script.js

---
+++ inc/footer.php
@@ -0,0 +1,31 @@
+<!-- ========== Footer Section ========== -->
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h3>Photographie</h3>
+                <p>La vie est un succession d'instants et de rencontres que seule la photographie à le pouvoir d'immortaliser.</p>
+                <div class="social-links">
+                    <a href="#" class="facebook"><i class="fab fa-facebook-square fa-3x"></i></a>
+                    <a href="#" class="instagram"><i class="fab fa-instagram-square fa-3x"></i></a>
+                </div>
+                <!-- <div class="copyright">
+                    &copy; Copyright<stronge><span>Charles Cantin</span></stronge>. Tous les droits sont réservés
+                </div> -->
+            </div>
+            <div class="col-lg-6 logo">
+                <img src="img/logo.JPG" alt="">
+                <div class="copyright">
+                    &copy; <stronge><span> 2021 Charles Cantin</span></stronge>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--  End Footer Section  -->
+
+<script src="js/portfolio-script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ inc/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>“Charles Cantin - Photographe”</title>
+
+    <!-- ========== Google fonts ========== -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- ========== Local stylesheet ========== -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/home-page-style.css">
+    <link rel="stylesheet" href="css/portfolio-style.css">
+
+    <!-- ========== Bootsrap CDN stylesheet ========== -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- ========== Font-awesome CDN ========== -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <!-- ========== Header Section ========== -->
+    <div class="container-fluid fixed-top bg-dark header-transparent" id="header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container mt-0">
+                <a href="index.php" class="navbar-brand fs-5 text-white">Charles Cantin</a>
+                <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon text-dark"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-right mb-2 mb-lg-0 ms-auto fs-5">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link text-white" aria-current="page">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="galerie.php" class="nav-link text-white">Galerie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="tarifs.php" class="nav-link text-white">Tarifs et prestations</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="contact.php" class="nav-link text-white">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!--  End Header Section  -->
